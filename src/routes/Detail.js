3175bfaee293b250407d82b0c84fb10492c47142
jubi-in <thetruthrunswild@naver.com>
2021-01-18 17:38:24 +0900

    real finish!!!

---
+++ src/routes/Detail.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push("/");
+        }
+    }
+    
+    render() {
+        const { location } = this.props;
+        if (location.state) {
+            return (
+                <section className="container">
+                    <div className="detail">
+                    <img src={location.state.poster} alt={location.state.title} title={location.state.title} />
+                        <div className="detail__data">
+                            <h1 className="detail__title">{location.state.title}</h1>
+                            <h3 className="detail__year">{location.state.year}</h3>
+                            <ul className="detail__genres">
+                                {location.state.genres.map((genre, index) => (
+                                    <li key={index} className="genres__genre">
+                                        {genre}
+                                    </li>
+                                ))}
+                            </ul>
+                            <p className="detail__summary">{location.state.summary}</p>
+                        </div>
+                    </div>
+                </section>
+            );
+        }
+        else {
+            return null;
+        }
+    }
+}
+export default Detail;
